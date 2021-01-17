0b55e1a4150b40a98ba09e7a97d63eb135d831d8
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2021-01-17 21:06:49 +0500

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,3 @@
-var body = document.getElementById('body');
-var div = document.createElement('div');
-function loader() {
-    body.appendChild(div);
-    div.setAttribute('id', #loader);
-    div.appendChild(Image);
-    Image.src('/img/a2dc9668f2cf170fe3efeb263128b0e7.gif')
-
+function myfunction() {
+    document.getElementById('loading').style.display = 'none';
 }
-loader();
