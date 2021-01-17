4a28af8da3cd091548b67028e4cbf6d87de8f80d
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2021-01-17 16:47:16 +0500

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -2,7 +2,7 @@ var body = document.getElementById('body');
 var div = document.createElement('div');
 function loader() {
     body.appendChild(div);
-    div.setAttribute('id', loader);
+    div.setAttribute('id', #loader);
     div.appendChild(Image);
     Image.src('/img/a2dc9668f2cf170fe3efeb263128b0e7.gif')
 
